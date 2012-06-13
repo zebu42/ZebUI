6ae7dc09480479b0d0a9b4a0ebc2fca577a33ed5
Zebu42 <Zebu42@gmail.com>
2012-06-13 16:25:58 +0200

    replace double quotes by singles to make them simple strings

--- zui-jquery/ZuiJqueryGrailsPlugin.groovy
+++ zui-jquery/ZuiJqueryGrailsPlugin.groovy
@@ -1,20 +1,20 @@
 class ZuiJqueryGrailsPlugin {
 
-    def version = "1.7.2"
+    def version = '1.7.2'
 
-    def grailsVersion = "2.0 > *"
+    def grailsVersion = '2.0 > *'
 
     def pluginExcludes = [
-        "grails-app/views/error.gsp"
+        'grails-app/views/error.gsp'
     ]
 
-    def title = "ZebUI JQuery Plugin"
-    def author = "Zebu42"
-    def authorEmail = "Zebu42@gmail.com"
-    def description = "ZebUI JQuery Plugin"
+    def title = 'ZebUI JQuery Plugin'
+    def author = 'Zebu42'
+    def authorEmail = 'Zebu42@gmail.com'
+    def description = 'ZebUI JQuery Plugin'
 
-    def license = "GPL3"
-    def documentation = "https://github.com/zebu42/ZebUI/wiki"
-    def issueManagement = [ url: "https://github.com/zebu42/ZebUI/issues" ]
-    def scm = [ url: "https://github.com/zebu42/ZebUI" ]
+    def license = 'GPL3'
+    def documentation = 'https://github.com/zebu42/ZebUI/wiki'
+    def issueManagement = [ url: 'https://github.com/zebu42/ZebUI/issues' ]
+    def scm = [ url: 'https://github.com/zebu42/ZebUI' ]
 }
