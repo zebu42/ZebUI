cb77ee5919058136515dff76ebe3824adc07f2bb
Zebu42 <Zebu42@gmail.com>
2012-06-14 17:11:56 +0200

    create the JQuery Resources artifact with the Google CDN wrapper from HTML5 BP

---
+++ zui-jquery/grails-app/conf/JQueryResources.groovy
@@ -0,0 +1,15 @@
+
+import org.codehaus.groovy.grails.commons.ApplicationHolder as AH
+
+def jqueryVersion = AH.application.mainContext.pluginManager.getGrailsPlugin('zui-jquery').version
+
+def jqueryWrapper = {
+  """<script src="//ajax.googleapis.com/ajax/libs/jquery/$jqueryVersion/jquery.min.js"></script>
+        <script>window.jQuery || document.write('${it.replace('</', '<\\/')}')</script>"""
+}
+
+modules = {
+  jquery {
+    resource url: "/js/jquery-${jqueryVersion}.min.js", wrapper: { jqueryWrapper(it) }
+  }
+}
