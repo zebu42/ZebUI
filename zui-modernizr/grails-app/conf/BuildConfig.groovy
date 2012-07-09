aafb8d92c4ee0d7316f93fae0860ae2aa87f6652
Zebu42 <Zebu42@gmail.com>
2012-07-09 12:57:29 +0200

    integrate the new Zui Resources plugin in the JQuery and Modernizr ones

--- zui-modernizr/grails-app/conf/BuildConfig.groovy
+++ zui-modernizr/grails-app/conf/BuildConfig.groovy
@@ -11,12 +11,9 @@ grails.project.dependency.resolution = {
 
     plugins {
         compile ':zui-utils:0.1'
+        runtime ':zui-resources:0.1'
 
         build(':release:2.0.3') { export = false }
         build(":tomcat:$grailsVersion") { export = false }
-
-        runtime(':resources:1.2-RC1') { export = false }
-        runtime(':zipped-resources:1.0') { export = false }
-        runtime(':cached-resources:1.0', ':cache-headers:1.1.5') { export = false }
     }
 }
