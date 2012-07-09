57cce7552773f7779671a501ef1d1eb98773735a
Zebu42 <Zebu42@gmail.com>
2012-07-09 12:51:33 +0200

    add all of the Resources framework dependencies to have them in a single place

--- zui-resources/grails-app/conf/BuildConfig.groovy
+++ zui-resources/grails-app/conf/BuildConfig.groovy
@@ -9,6 +9,10 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
+        runtime ':resources:1.2-RC1'
+        runtime ':zipped-resources:1.0'
+        runtime ':cached-resources:1.0', ':cache-headers:1.1.5'
+
         build(':release:2.0.3') { export = false }
     }
 }
